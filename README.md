772b0102ff14f83d92b12859f4f690520ad18f42
Alex Walling <alexwalling@Alexs-MacBook-Pro.local>
2017-06-16 12:16:51 -0700

    updates

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-Setting up React Router
+# Rapid Todo React Website
+
+## Setting up React Router
 https://medium.com/@patriciolpezjuri/using-create-react-app-with-react-router-express-js-8fa658bf892d
 
+https://medium.com/@patriciolpezjuri/using-create-react-app-with-react-router-express-js-8fa658bf892d
+
+
+## Starting the server
+npm run build
+node server
+
+
+
+
+# TODO:
+- [x] Build simple react components
+- [x] Host on AWS
+- [ ] setup mongo DB
--- server/index.js
+++ server/index.js
@@ -1,9 +1,34 @@
 'use strict';
 
-const app = require('./app');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+var router = express.Router();
 
 const PORT = process.env.PORT || 9000;
 
+/*
+LOG HTTP Requests
+*/
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+
+/*
+Serving static assets build from running 'npm run build'
+*/
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+/*
+Serving based on routes defined in src/routes.js
+*/
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+/*
+Listening on defined PORT
+*/
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
